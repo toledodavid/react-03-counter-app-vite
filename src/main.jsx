ee73282beafbebb31c2a32b26fedc6dd8af0d252
David Toledo <dtoledoz0207@gmail.com>
2023-02-07 12:21:06 -0600

    Create styles.css file to add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { HelloWorldApp } from './HelloWorldApp';
 
+import './styles.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
