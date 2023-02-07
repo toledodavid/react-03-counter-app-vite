5693828d932b0464be343a95b218f70105bb5547
David Toledo <dtoledoz0207@gmail.com>
2023-02-07 11:18:56 -0600

    Print js variables and code in html elements

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,25 @@
 // import { Fragment } from "react";
 
+const newMessage = {
+  title: 'Hello',
+  message: 'David'
+};
+
+const greeting = () => (<p>Good morning!</p>);
+
+const getResult = (a, b) => a + b;
+
 export const FirstApp = () => {
   return (
     <>
       <h1>First App</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, quas voluptatem. Consequuntur neque quos hic quod officiis numquam doloribus facilis sapiente quis nemo sit reprehenderit fuga, minima alias quibusdam natus.</h3>
+
+      <code>{JSON.stringify(newMessage)}</code>
+
+      { greeting() }
+
+      <p>Result: { getResult(1, 6) }</p>
     </>
   );
 
