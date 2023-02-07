c0bd08c3f71da03d0a926ee161c6801058d34f4a
David Toledo <dtoledoz0207@gmail.com>
2023-02-07 11:00:40 -0600

    Use Fragment in FirstApp component to return two elements

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,17 @@
+// import { Fragment } from "react";
 
 export const FirstApp = () => {
   return (
-    <h1>First App</h1>
+    <>
+      <h1>First App</h1>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, quas voluptatem. Consequuntur neque quos hic quod officiis numquam doloribus facilis sapiente quis nemo sit reprehenderit fuga, minima alias quibusdam natus.</h3>
+    </>
   );
+
+  // return (
+  //   <Fragment>
+  //     <h1>Hi</h1>
+  //     <p>This is a paragraph</p>
+  //   </Fragment>
+  // );
 }
