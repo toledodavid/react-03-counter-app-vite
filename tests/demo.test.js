8cf4fb2736991a4e9ee02980de0775fd6d575b18
David Toledo <dtoledoz0207@gmail.com>
2023-02-22 11:24:13 -0600

    Install @types/jest and create first test for demo test suite

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,14 @@
 
 
-test('It must pass', () => {
+describe('Demo tests', () => {
+  test('It must pass', () => {
+    // 1. Initialization
+    const message1 = 'Hello world';
   
+    // 2. Act
+    const message2 = message1.trim();
+  
+    // 3. Assertion
+    expect(message1).toBe(message2);
+  });
 });
