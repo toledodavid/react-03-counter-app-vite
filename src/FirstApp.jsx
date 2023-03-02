5b0bd3e3aeaf29ef01aeab4455e8404a12630e19
David Toledo <dtoledoz0207@gmail.com>
2023-03-02 10:59:26 -0600

    Using getByTestId method to get html element to test its content

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -13,7 +13,7 @@ const getResult = (a, b) => a + b;
 export const FirstApp = ({title, subTitle, name}) => {
   return (
     <>
-      <h1>{title}</h1>
+      <h1 data-testid="test-title">{title}</h1>
       <h3>{subTitle}</h3>
       <p>{name}</p>
 
--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -4,19 +4,28 @@ import { FirstApp } from '../src/FirstApp';
 
 describe('Tests for <FirstApp />', () => {
 
-  test('should match with the snapshot', () => {
-    const { container } = render(<FirstApp />);
-    expect(container).toMatchSnapshot();
-  });
+  // test('should match with the snapshot', () => {
+  //   const { container } = render(<FirstApp />);
+  //   expect(container).toMatchSnapshot();
+  // });
 
   test('should show the title in an h1 element', () => {
     const title = 'Hi, I am David';
-    const {container, getByText} = render(<FirstApp title={title}/>);
+    const {container, getByText, getByTestId} = render(<FirstApp title={title}/>);
 
-    const h1 = container.querySelector('h1');
+    // const h1 = container.querySelector('h1');
 
-    expect(getByText(title)).toBeTruthy();
-    expect(h1.innerHTML).toContain(title);
+    // expect(getByText(title)).toBeTruthy();
+    // expect(h1.innerHTML).toContain(title);
+
+    expect(getByTestId('test-title').innerHTML).toContain(title);
+  });
+
+  test('should show a subtitle sent by props', () => {
+    const subTitle = 12;
+    const {getByText} = render(<FirstApp subTitle={subTitle}/>);
+
+    expect(getByText(subTitle)).toBeTruthy();
   });
 
 });
