dee634423258c03fedc776ae3756dd69c550de30
David Toledo <dtoledoz0207@gmail.com>
2023-03-02 10:29:01 -0600

    Add first tests for FirstApp component

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -5,7 +5,18 @@ import { FirstApp } from '../src/FirstApp';
 describe('Tests for <FirstApp />', () => {
 
   test('should match with the snapshot', () => {
-    render(<FirstApp />);
+    const { container } = render(<FirstApp />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should show the title in an h1 element', () => {
+    const title = 'Hi, I am David';
+    const {container, getByText} = render(<FirstApp title={title}/>);
+
+    const h1 = container.querySelector('h1');
+
+    expect(getByText(title)).toBeTruthy();
+    expect(h1.innerHTML).toContain(title);
   });
 
 });
